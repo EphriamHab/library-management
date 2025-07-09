51ef68730e6eba0af68dc933d9046a89363a15b1
EphriamHab <ephremhabtamu2015@gmail.com>
2025-07-09 19:27:03 +0300

    store

---
+++ src/store/store.ts
@@ -0,0 +1,21 @@
+// src/store/index.ts
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from './api';
+import authSlice from './slices/authSlice';
+import uiSlice from './slices/uiSlice';
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    auth: authSlice,
+    ui: uiSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
